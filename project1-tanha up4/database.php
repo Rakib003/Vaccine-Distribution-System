da596fcff843c28eb629d5e113204b1a6f31023a
Rakib003 <74839882+Rakib003@users.noreply.github.com>
2021-06-19 01:32:01 +0600

    Delete project1-tanha up4 directory

--- project1-tanha up4/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$conn = oci_connect("project1","project1", "localhost/XE");
-if (!$conn) {
-	$e = oci_error();
-	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-}
-?>
